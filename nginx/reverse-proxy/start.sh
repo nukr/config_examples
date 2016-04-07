a21ab31831646f3fc681e375ea74cc600d5a47a1
Nukr <nukrs.w@gmail.com>
2016-04-07 14:41:41 +0800

    add start script

---
+++ nginx/reverse-proxy/start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+CONF_DIR=$(dirname $0)
+
+docker run \
+       -d \
+       -p 80:80 \
+       -v ${CONF_DIR}/reverse-proxy.conf:/etc/nginx/conf.d/reverse-proxy.conf \
+       nginx:alpine
